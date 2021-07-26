3e9013832e5244e781ed77285b9df72e512dc506
finify <ifeanyiizuegbu@yahoo.com>
2021-07-26 20:37:12 +0100

    first commit

---
+++ includes/header.php
@@ -0,0 +1,28 @@
+<?php 
+require('settings.php'); 
+?>
+<!DOCTYPE HTML>
+<html lang="zxx">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="description" content="Orbitor,business,company,agency,modern,bootstrap4,tech,software">
+  <meta name="author" content="themefisher.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>FCVMCSL - Savings and Loan Scheme</title>
+
+  <!-- Favicon -->
+  <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico">
+
+  <!-- bootstrap.min css -->
+  <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+  <!-- Icon Font Css -->
+  <link rel="stylesheet" href="plugins/icofont/icofont.min.css">
+  <!-- Slick Slider  CSS -->
+  <link rel="stylesheet" href="plugins/slick-carousel/slick/slick.css">
+  <link rel="stylesheet" href="plugins/slick-carousel/slick/slick-theme.css">
+
+  <!-- Main Stylesheet -->
+  <link rel="stylesheet" href="css/style.css">
+
+<script type="text/javascript" charset="UTF-8" src="https://maps.googleapis.com/maps-api-v3/api/js/45/5/common.js"></script><script type="text/javascript" charset="UTF-8" src="https://maps.googleapis.com/maps-api-v3/api/js/45/5/util.js"></script>
+</head>
---
+++ includes/settings.php
@@ -0,0 +1,13 @@
+<?php
+
+$sitename = "metrocoin-miner";
+$siteurl = "#";
+$sitetitle = "metrocoin-miner - a smart way to Mine Crypto";
+
+//Administrator
+$siteauthor = "metrocoin-miner";
+$email_add = "support@metrocoin-miner.com";
+$location = "";
+
+
+?>
